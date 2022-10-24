ad34e7d0c3cae577d94d5ab23155ebab1d0c3366
Alen Šiljak <dev@alensiljak.eu.org>
2022-10-25 00:45:26 +0200

    routing on tap

--- src/routes/(viewer)/viewer/+page.svelte
+++ src/routes/(viewer)/viewer/+page.svelte
@@ -2,6 +2,7 @@
 	import abcjs, { type AbcVisualParams } from 'abcjs';
 	import { onMount } from 'svelte';
 	import { tap } from 'svelte-gestures';
+	import { goto } from '$app/navigation';
 
 	onMount(() => {
 		createSheet();
@@ -22,11 +23,36 @@ F c F c
 	}
 
 	function onTap(event: Event) {
-		console.log('tapped!');
+		//console.log('tapped!');
+		// Open dialog
+		// add "open" attribute to the dialog
+		//alert('yo!')
+
+		// just go back, for now
+		goto(`/`, { replaceState: true }) 
 	}
 </script>
 
 <div id="paper" use:tap={{ timeframe: 300 }} on:tap={onTap} />
+
+<dialog>
+	<article>
+	  <header>
+		<a href="#close" aria-label="Close" class="close"></a>
+		Modal title
+	  </header>
+	  <p>
+		Nunc nec ligula a tortor sollicitudin dictum in vel enim. 
+		Quisque facilisis turpis vel eros dictum aliquam et nec turpis. 
+		Sed eleifend a dui nec ullamcorper. 
+		Praesent vehicula lacus ac justo accumsan ullamcorper.
+	  </p>
+	  <footer>
+		<a href="#cancel" role="button" class="secondary">Cancel</a>
+		<a href="#confirm" role="button">Confirm</a>
+	  </footer>
+	</article>
+  </dialog>
 
 <style lang="css">
 	#paper {
