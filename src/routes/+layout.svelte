70ef1c7374322b0db1d68c1b35a3fb209c7b4520
Alen Šiljak <dev@alensiljak.eu.org>
2022-10-24 23:48:47 +0200

    adding other dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
-<div>
-    This is layout
-</div>
+<script lang="ts">
+	import '@picocss/pico/css/pico.css';
+</script>
 
-<slot></slot>
+<header>This is the layout</header>
+
+<main class="container">
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-
+<h1>Welcome to Stage Dive!</h1>
+<p>This is a music set-list application and an editor.</p>
 <p>Test some navigation</p>
 
 <ul>
