59d53f99247d25c2d9c82eff80d04905fe25fdf4
Alen Šiljak <dev@alensiljak.eu.org>
2022-10-25 12:24:14 +0200

    using a pre-release of vite-plugin-pwa for sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-//import { VitePWA } from 'vite-plugin-pwa'
-// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 const config: UserConfig = {
 	plugins: [sveltekit(), 
-		// VitePWA()
-		// SvelteKitPWA()
+		SvelteKitPWA()
 	]
 };
 
